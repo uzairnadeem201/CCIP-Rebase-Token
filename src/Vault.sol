383ba5dd24c86c9eecac04e520efe0e4fbc5a6f0
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-05-07 15:53:06 +0500

    Created Vault

---
+++ src/Vault.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {IRebaseToken} from "./interfaces/iRebaseToken.sol";
+contract Vault {
+    IRebaseToken private immutable i_rebaseToken;
+    event Deposit(address indexed sender, uint256 amount);
+    event Redeem(address indexed sender, uint256 amount);
+    error Vault__RedeemFailed();
+    constructor(IRebaseToken rebaseToken) {
+        i_rebaseToken = rebaseToken;
+    }
+    function getRebaseTokenAddress() external view returns (address) {
+        return address(i_rebaseToken);
+    }
+    receive() external payable {}
+    function deposit() external payable{
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender,msg.value);
+    }
+    function redeem(uint256 _amount) external{
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender,_amount);
+    }
+
+}
