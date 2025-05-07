5aabe6eebd09f3ce9eb9f8403a9be1feb8871ff4
uzairnadeem201 <uzairnadeem201@gmail.com>
2025-05-07 18:46:11 +0500

    Did Fuzz Testing

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import {IRebaseToken} from "./interfaces/iRebaseToken.sol";
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 contract Vault {
     IRebaseToken private immutable i_rebaseToken;
     event Deposit(address indexed sender, uint256 amount);
@@ -14,10 +14,13 @@ contract Vault {
     }
     receive() external payable {}
     function deposit() external payable{
-        i_rebaseToken.mint(msg.sender, msg.value);
+        i_rebaseToken.mint(msg.sender, msg.value,i_rebaseToken.getInterestRate());
         emit Deposit(msg.sender,msg.value);
     }
     function redeem(uint256 _amount) external{
+        if(_amount == type(uint256).max){
+            _amount = i_rebaseToken.balanceOf(msg.sender);
+        }
         i_rebaseToken.burn(msg.sender, _amount);
         (bool success,) = payable(msg.sender).call{value: _amount}("");
         if (!success) {
--- src/interfaces/iRebaseToken.sol
+++
@@ -1,6 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-interface IRebaseToken {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
-}
